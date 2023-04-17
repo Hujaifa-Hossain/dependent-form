0e5d1aceb6f998790721d12b7b6822aecc6d3293
Hujaifa21 <21hujaifa@gmail.com>
2023-04-17 20:47:38 +0600

    form data shown in alert

---
+++ README.md
@@ -0,0 +1,13 @@
+##### Dependent form using React.js & React Hook Form
+
+##### Hosted @ Netlify
+https://dependent-form.netlify.app/
+
+##### Installation
+
+* First download the source code and then run these commands.
+```bash
+  cd dependent-form
+  npm install 
+  npm run dev
+```
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,20 @@
 import { useForm } from "react-hook-form";
 
 const Form = () => {
+
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => alert(JSON.stringify(data));
 
-  const current = watch("current");
+  const situation = watch("situation");
   const institution = watch("institution");
-  console.log(current, institution);
+  const work = watch("work");
 
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center my-4">
       <form onSubmit={handleSubmit(onSubmit)}>
         <p className="m-0 fw-bold">Are you currently studying?</p>
 
@@ -24,7 +24,7 @@ const Form = () => {
             <input
               type="radio"
               value="study"
-              {...register("current", { required: true })}
+              {...register("situation", { required: true })}
             />
             <span>Yes, I am studying</span>
           </div>
@@ -32,14 +32,14 @@ const Form = () => {
             <input
               type="radio"
               value="job"
-              {...register("current", { required: true })}
+              {...register("situation", { required: true })}
             />
             <span>No, I am not studying</span>
           </div>
         </div>
 
         {/* if study  */}
-        {current === "study" && (
+        {situation === "study" && (
           <div>
             <p className="m-0 fw-bold">Are you currently?</p>
 
@@ -69,7 +69,7 @@ const Form = () => {
           <div>
             <div>
               <input
-              placeholder="Your school name"
+                placeholder="Your school name"
                 className="w-100 p-1 my-1"
                 type="type"
                 {...register("school-name", { required: true })}
@@ -88,13 +88,13 @@ const Form = () => {
             </div>
           </div>
         )}
-        
+
         {/* if institution is college  */}
         {institution === "college" && (
           <div>
             <div>
               <input
-              placeholder="Your college name"
+                placeholder="Your college name"
                 className="w-100 p-1 my-1"
                 type="type"
                 {...register("college-name", { required: true })}
@@ -122,8 +122,106 @@ const Form = () => {
           </div>
         )}
 
+        {/* if not study  */}
+        {situation === "job" && (
+          <div>
+            <p className="m-0 fw-bold">Are you currently?</p>
+
+            <div className="d-flex justify-content-between">
+              <div className="m-1">
+                <input
+                  type="radio"
+                  value="looking"
+                  {...register("work", { required: true })}
+                />
+                <span>Looking for a job?</span>
+              </div>
+              <div className="m-1">
+                <input
+                  type="radio"
+                  value="working"
+                  {...register("work", { required: true })}
+                />
+                <span>Currently working</span>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* if looking for job  */}
+        {work === "looking" && (
+          <div>
+            <div>
+              <input
+                placeholder="Your college name"
+                className="w-100 p-1 my-1"
+                type="type"
+                {...register("college-name", { required: true })}
+              />
+
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="m-0">Select your latest degree</p>
+                <select {...register("degree")}>
+                  <option value="BTech">BTech</option>
+                  <option value="bsc">BSC</option>
+                  <option value="bba">BBA</option>
+                  <option value="mba">MBA</option>
+                </select>
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="m-0">Select your graduation year</p>
+                <select {...register("year")}>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                  <option value="2023">2023</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* if working */}
+        {work === "working" && (
+          <div>
+            <div>
+              <input
+                placeholder="Your company name"
+                className="w-100 p-1 my-1"
+                type="type"
+                {...register("company-name", { required: true })}
+              />
+
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="m-0">Select year of experience</p>
+                <select {...register("experience")}>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                  <option value="4">Four</option>
+                  <option value="5">Five</option>
+                  <option value="6">Six</option>
+                  <option value="7">Seven</option>
+                  <option value="8">Eight</option>
+                  <option value="9">Nine</option>
+                </select>
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="m-0">Select your job title</p>
+                <select {...register("job-title")}>
+                  <option value="frontend">Frontend developer</option>
+                  <option value="fullstack">Full Stack developer</option>
+                  <option value="ai">AI developer</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        )}
+
         <input className="w-100 bg-success my-1" type="submit" />
       </form>
+
     </div>
   );
 };
